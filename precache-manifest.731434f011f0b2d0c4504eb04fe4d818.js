c06c3340620b48bf91072bfddac5bb077f07e0a5
eunjin0212 <eunjin9639@gmail.com>
2021-08-13 06:12:21 +0900

    Updates

---
+++ precache-manifest.731434f011f0b2d0c4504eb04fe4d818.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7562eeda46b6e1a673dfbd19a8102902",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5545d57fd8d47b013545",
+    "url": "./static/css/main.e8cc4d1e.chunk.css"
+  },
+  {
+    "revision": "1443b38994acda68f375",
+    "url": "./static/js/2.c217bb06.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "./static/js/2.c217bb06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5545d57fd8d47b013545",
+    "url": "./static/js/main.e6e87723.chunk.js"
+  },
+  {
+    "revision": "e31a5c0dd6ffeef954a6",
+    "url": "./static/js/runtime-main.7ccf5ded.js"
+  }
+]);
